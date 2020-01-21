395b8510c56063e60c9fc872c2e68360d0b3fe65
Cathal Ryan <cathalryan92@gmail.com>
2020-01-21 16:33:37 -0500

    Dashboard to display data

---
+++ dashboard-client/src/Graph.js
@@ -0,0 +1,54 @@
+
+import React, {Component} from 'react';
+import { Container,Grid } from '@material-ui/core';
+import {
+
+BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Label, Text
+} from 'recharts';
+const BarComponent = (length) => {length ? <div><Tooltip /><Bar dataKey="count" fill="#1f416f"/></div>: null}
+class Graph extends Component{
+constructor(props){
+super(props)
+console.log("constructor")
+}
+componentDidUpdate(prevProps){
+
+if(this.props.data !== prevProps.data){
+console.log("updated")
+}
+}
+shouldComponentUpdate(nextProps){
+	if(nextProps.noOfMessages % 8 == 0){	
+return true
+}
+return false
+}
+
+render(){
+return (<div className="graphContainer"><BarChart
+        width={500}
+        height={300}
+        data={this.props.data.length?this.props.data:[{"word":"", "count":0}]}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      ><CartesianGrid strokeDasharray="3 3" horizontal={false} vertical={false} />
+{this.props.XAxis}
+        <YAxis allowDecimals={false} label={<Text
+      x={0}
+      y={0}
+      dx={50}
+      dy={150}
+      offset={0}
+      angle={-90}
+   >Count</Text>}/>
+{this.props.data.length ? <Tooltip/>:null}
+{this.props.data.length ? <Bar dataKey="count" fill="#1f416f"/>:null}
+
+      </BarChart></div>
+)}
+
+
+}
+
+export default Graph;
