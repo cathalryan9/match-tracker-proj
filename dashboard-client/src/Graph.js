a781702396816cf1c9079f7b9a27c29c88f32698
Cathal Ryan <cathalryan92@gmail.com>
2020-01-24 14:20:09 -0500

    Add graph for hashtag totals

--- dashboard-client/src/Graph.js
+++ dashboard-client/src/Graph.js
@@ -18,12 +18,10 @@ constructor(props){
 }
 componentDidUpdate(prevProps){
 	if(this.props.data !== prevProps.data){
-		console.log("updated")
 	}
 }
 shouldComponentUpdate(nextProps){
 	if(nextProps.data.data.length){
-		console.log("will update")	
 		return true
 	}
 	return false
@@ -31,15 +29,18 @@ shouldComponentUpdate(nextProps){
 
 render(){
 
-return (<div className="graphContainer"><BarChart
-        width={500}
+return (<div className="graph-box">
+	<Grid container direction="column" alignItems="center" spacing={0}>
+	<Grid item className="graph-title-bar">{this.props.data.graphTitle}</Grid>
+	<Grid item className="graph-container"><BarChart
+        width={600}
         height={300}
         data={this.props.data.data.length?this.props.data.data:[{"word":"", "count":0}]}
         margin={{
           top: 5, right: 30, left: 20, bottom: 5,
         }}
       ><CartesianGrid strokeDasharray="3 3" horizontal={false} vertical={false} />
-	<XAxis interval={0} dataKey={this.props.data.xKey.toString()} tick={this.props.data.tick == "true"}/>
+	<XAxis interval={0} dataKey={this.props.data.xKey.toString()} tick={this.props.data.tick == "true"} interval={this.props.data.data.length < 10 ? 0 : this.props.data.data.length < 45? 5: 10}/>
         <YAxis allowDecimals={false} label={<Text
       x={0}
       y={0}
@@ -51,7 +52,7 @@ return (<div className="graphContainer"><BarChart
 {this.props.data.data.length ? <Tooltip content={<CustomTooltip/>}/>:null}
 {this.props.data.data.length ? <Bar dataKey={this.props.data.yKey.toString()} fill="#1f416f"/>:null}
 
-      </BarChart></div>
+      </BarChart></Grid></Grid></div>
 )}
 
 
