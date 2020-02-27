370d9ed14ee89e3a03b33ddc774e9938719b687f
Cathal Ryan <cathalryan92@gmail.com>
2020-02-26 21:46:24 -0500

    Add websocket for hashtag

--- dashboard-client/src/App.js
+++ dashboard-client/src/App.js
@@ -3,7 +3,9 @@ import { Container,Grid } from '@material-ui/core';
 import Graph from './Graph';
 import Metrics from './Metrics';
 import {XAxis, Label} from 'recharts';
-const ws = new WebSocket('ws://10.0.2.2:8080/websocketserver-0.0.1-SNAPSHOT/word_count/10005');
+const wordCountws = new WebSocket('ws://10.0.2.2:8080/websocketserver-0.0.1-SNAPSHOT/word_count/10005');
+const hashtagCountws = new WebSocket('ws://10.0.2.2:8080/websocketserver-0.0.1-SNAPSHOT/hashtag_count/10005');
+
 const dataMap = {};
 var currentMinuteMap = {};
 
@@ -24,33 +26,34 @@ class App extends Component{
 
 
 	componentDidMount() {
-	        ws.onopen = () => {
-        	// on connecting, do nothing but log it to the console
-	        console.log('connected')
+	        wordCountws.onopen = () => {
+		        console.log('connected')
         	}
-	ws.onmessage = evt => {
-	console.log('new message')
-        // listen to data sent from the websocket server
-	var t0 = performance.now()
-        const message = JSON.parse(evt.data)
-	const jsonMessage = JSON.parse(message.content)
-	var word = jsonMessage["text"]
-	console.log(jsonMessage)
-	this.setState({freqData:{"graphTitle":"Total Word Count (Top 30)","yKey":"count","xKey":"word","tick": "false", "data":jsonMessage}})
-//			console.log(t3-t2)
+		wordCountws.onmessage = evt => {
+		        // listen to data sent from the websocket server
+        		const message = JSON.parse(evt.data)
+			const jsonMessage = JSON.parse(message.content)
+			var word = jsonMessage["text"]
+			this.setState({freqData:{"graphTitle":"Total Word Count (Top 30)","yKey":"count","xKey":"word","tick": "false", "data":jsonMessage}})
+	        }
+		wordCountws.onerror = () => {
+			console.log('error with ws')
+		}
 
-		
-        }
-
-        ws.onclose = () => {
-        console.log('disconnected')
-        // automatically try to reconnect on connection loss
-
-        }
-	ws.onerror = () => {
-	console.log('error with ws')
+                hashtagCountws.onopen = () => {
+                        console.log('connected')
+                }
+                hashtagCountws.onmessage = evt => {
+                        // listen to data sent from the websocket server
+                        const message = JSON.parse(evt.data)
+                        const jsonMessage = JSON.parse(message.content)
+                        var word = jsonMessage["text"]
+                        this.setState({hashtagData:{"graphTitle":"Hashtag unt (Top 30)","yKey":"count","xKey":"word","tick":"false", "data":jsonMessage}})
+                }
+                hashtagCountws.onerror = () => {
+                        console.log('error with ws')
+                }
 	}
-}
 	render(){
 	return <Container className="app-container">
 		<h1 className="title-name">Twitter Feed Dashboard</h1>
